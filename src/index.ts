7dd5c314047acc10ffeb3c9c22e2315a2230cf19
ulxsth <ulxsth@gmail.com>
2024-09-06 10:13:30 +0900

    chore: express せっとあっぷ

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("hello world");
+import express from 'express'
+
+const app = express()
+const port = 3000
+
+app.get("/", (req, res) => {
+  res.send("Hello World!")
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+}).on('error', (err) => {
+  throw new Error(err.message)
+})
