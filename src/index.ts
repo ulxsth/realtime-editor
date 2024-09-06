8e835427a175e9a4f20b67d2f7cbc1f4df028417
ulxsth <ulxsth@gmail.com>
2024-09-06 11:24:22 +0900

    feat: socketio の初期化処理を追加

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,28 @@
 import express from 'express'
+import { Server } from 'socket.io'
+import { createServer } from 'http'
 
 const app = express()
-const port = 3000
-
 app.use(express.static('public'))
+
+const server = createServer(app)
+const io = new Server(server)
+
 
 app.get("/", (req, res) => {
   return express.static('public/index.html')
 })
 
-app.listen(port, () => {
+io.on("connection", (socket) => {
+  console.log("A user connected: ", socket.id)
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected")
+  })
+})
+
+const port = 3000
+server.listen(port, () => {
   console.log(`Server is running on port ${port}`)
 }).on('error', (err) => {
   throw new Error(err.message)
