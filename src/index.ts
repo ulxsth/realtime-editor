d1c7d7e23485d0bc9716866fc847a150167d28d6
ulxsth <ulxsth@gmail.com>
2024-09-06 10:58:18 +0900

    feat: ace editor を追加

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,10 @@ import express from 'express'
 const app = express()
 const port = 3000
 
+app.use(express.static('public'))
+
 app.get("/", (req, res) => {
-  res.send("Hello World!")
+  return express.static('public/index.html')
 })
 
 app.listen(port, () => {
